2146f79823b9fcd7b003f7d1a797831cde5bb454
ksdaylight <yuanmiwen111@163.com>
2023-09-15 05:23:57 -0400

    feat: add script

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    apps: [
+        {
+            name: 'nest-api',
+            script: 'node_modules/nx/bin/nx.js',
+            args: 'run nest-api:serve:production',
+            instances: 2,
+            exec_mode: 'cluster',
+            out_file: './logs/nest-api-out.log',
+            error_file: './logs/nest-api-error.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+        {
+            name: 'frontend',
+            script: 'node_modules/nx/bin/nx.js',
+            args: 'run frontend:serve:production',
+            instances: 1,
+            out_file: './logs/frontend-out.log',
+            error_file: './logs/frontend-error.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+    ],
+};
