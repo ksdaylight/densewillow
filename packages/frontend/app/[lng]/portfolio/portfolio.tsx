fbb2741fd834ed9abd49957c75baa4b8052a1696
ksdaylight <yuanmiwen111@163.com>
2023-09-22 00:02:57 -0400

    feat: add portfolio home structure

--- packages/frontend/app/[lng]/portfolio/portfolio.tsx
+++ packages/frontend/app/[lng]/portfolio/portfolio.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import { Sidebar } from '@frontend/components/common/Siderbar';
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import Typed from 'typed.js';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FaLinkedinIn } from 'react-icons/fa';
+import { BiLogoFacebook, BiLogoStackOverflow } from 'react-icons/bi';
+import { AiFillGithub } from 'react-icons/ai';
 
 const navItems = [
     { href: '#home', icon: 'home', label: 'Home' },
@@ -14,15 +21,124 @@ const navItems = [
 interface Props {}
 const PortfolioClient: FC<Props> = (): JSX.Element => {
     const [currentSection, setCurrentSection] = useState<string | null>('#home');
+    const el = useRef<HTMLHeadingElement>(null);
     const handleSectionChange = (sectionId: string) => {
         setCurrentSection(sectionId);
     };
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['Front-End web developer', 'Back-End web developer', 'Web designer'],
+            loop: true,
+            typeSpeed: 70,
+            backSpeed: 10,
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
     return (
         <div className="min-h-screen bg-dark_bg ">
             <Sidebar navItems={navItems} onSectionChange={handleSectionChange} />
             <main className="pl-0 pt-[50px] md:pl-[102px] md:pt-0 ">
-                <section className={`bg-[#666] ${currentSection === '#home' ? 'block' : 'hidden'}`}>
-                    Home
+                <section
+                    className={`${
+                        currentSection === '#home' ? 'block' : 'hidden'
+                    } bg-[url('/images/bg-shape.png')] bg-no-repeat bg-[40vw_-24px]`}
+                >
+                    <div className="container mx-auto">
+                        <div className="flex flex-wrap">
+                            <div className="w-full lg:w-1/2 ">
+                                <div id="info" className="pt-[281px]">
+                                    <Image
+                                        src="/images/person-circle.png"
+                                        alt="person circle"
+                                        width={45}
+                                        height={45}
+                                        className="mb-[24px]"
+                                    />
+                                    <h1 className="mb-[24px]">
+                                        <span className="text-gray">I’m</span> Mojtaba,
+                                    </h1>
+                                    <div className="flex  items-center mb-[16px]">
+                                        <h2 ref={el} className="text-gray mr-[2px]">
+                                            Front-End web developer
+                                        </h2>
+                                    </div>
+                                    <p className="max-w-[430px] mb-[74px]">
+                                        I design and develop services for customers of all sizes,
+                                        specializing in creating stylish, modern websites, web
+                                        services, and online stores.
+                                    </p>
+                                    <a
+                                        href="assets/files/cv.pdf"
+                                        download
+                                        className="btn-custom-blue"
+                                    >
+                                        Download CV
+                                    </a>
+                                    <div className="socials mt-[191px]">
+                                        <ul className="flex m-0 p-0 list-none">
+                                            <li className="mr-[8px]">
+                                                <Link
+                                                    href="#"
+                                                    className="flex items-center no-underline transition-all duration-[0.3s] px-[11px] py-[9px] hover:bg-secondary-dark hover:rounded-[6px] group/link"
+                                                >
+                                                    <FaLinkedinIn className="text-[18px] fill-gray transition-all duration-[0.3s] mr-2 group-hover/link:fill-white" />
+                                                    <span className="text-gray transition-all duration-[0.3s] group-hover/link:fill-white">
+                                                        Linkedin
+                                                    </span>
+                                                </Link>
+                                            </li>
+                                            <li className="mr-[8px]">
+                                                <Link
+                                                    href="#"
+                                                    className="flex items-center no-underline transition-all duration-[0.3s] px-[11px] py-[9px] hover:bg-secondary-dark hover:rounded-[6px] group/link"
+                                                >
+                                                    <BiLogoFacebook className="text-[18px] fill-gray transition-all duration-[0.3s] mr-2 group-hover/link:fill-white" />
+                                                    <span className="text-gray transition-all duration-[0.3s] group-hover/link:fill-white">
+                                                        Facebook
+                                                    </span>
+                                                </Link>
+                                            </li>
+                                            <li className="mr-[8px]">
+                                                <Link
+                                                    href="#"
+                                                    className="flex items-center no-underline transition-all duration-[0.3s] px-[11px] py-[9px] hover:bg-secondary-dark hover:rounded-[6px] group/link"
+                                                >
+                                                    <AiFillGithub className="text-[18px] fill-gray transition-all duration-[0.3s] mr-2 group-hover/link:fill-white" />
+                                                    <span className="text-gray transition-all duration-[0.3s] group-hover/link:fill-white">
+                                                        Github
+                                                    </span>
+                                                </Link>
+                                            </li>
+                                            <li className="mr-[8px]">
+                                                <Link
+                                                    href="#"
+                                                    className="flex items-center no-underline transition-all duration-[0.3s] px-[11px] py-[9px] hover:bg-secondary-dark hover:rounded-[6px] group/link"
+                                                >
+                                                    <BiLogoStackOverflow className="text-[18px] fill-gray transition-all duration-[0.3s] mr-2 group-hover/link:fill-white" />
+                                                    <span className="text-gray transition-all duration-[0.3s] group-hover/link:fill-white">
+                                                        Stack Overflow
+                                                    </span>
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full lg:w-1/2  h-screen flex items-center justify-end">
+                                <Image
+                                    src="/images/person-home.png"
+                                    alt="person home"
+                                    className="transition-all duration-[0.3s] rounded-[8px] hover:saturate-[200%] hover:sepia-[1]"
+                                    width={546}
+                                    height={843}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </section>
                 <section
                     id="about"
@@ -56,12 +172,6 @@ const PortfolioClient: FC<Props> = (): JSX.Element => {
                 >
                     Contact
                 </section>
-                {/* <p className="inline-block text-5xl bg-[#a0a0aa] w-[400px] border-[20px] border-red-500 ">
-                    Test text
-                </p>
-                <h1>Cheetah Academy</h1>
-                <h2>Cheetah Academy</h2>
-                <p className="text-3xl">Paragraph</p> */}
             </main>
         </div>
     );
