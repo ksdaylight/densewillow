000b4df016713e5ffd3970818f7388a8d5e0f837
ksdaylight <yuanmiwen111@163.com>
2023-09-25 04:51:01 -0400

    feat:resume

--- packages/frontend/app/[lng]/portfolio/portfolio.tsx
+++ packages/frontend/app/[lng]/portfolio/portfolio.tsx
@@ -7,7 +7,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { FaLinkedinIn } from 'react-icons/fa';
+import { LiaUniversitySolid } from 'react-icons/lia';
 import { BiLogoStackOverflow } from 'react-icons/bi';
+import { BsBriefcase } from 'react-icons/bs';
 import { AiFillGithub } from 'react-icons/ai';
 import { random } from 'lodash';
 
@@ -21,7 +23,7 @@ const navItems = [
 ];
 interface Props {}
 const PortfolioClient: FC<Props> = (): JSX.Element => {
-    const [currentSection, setCurrentSection] = useState<string | null>('#about_me');
+    const [currentSection, setCurrentSection] = useState<string | null>('#my_resume');
     const el = useRef<HTMLHeadingElement>(null);
     const handleSectionChange = (sectionId: string) => {
         setCurrentSection(sectionId);
@@ -160,7 +162,7 @@ const PortfolioClient: FC<Props> = (): JSX.Element => {
                 </section>
                 <section
                     id="about_me"
-                    className={` ${
+                    className={`${
                         currentSection === '#about_me' ? 'block' : 'hidden'
                     } md:pt-[64px]`}
                 >
@@ -504,9 +506,135 @@ const PortfolioClient: FC<Props> = (): JSX.Element => {
                 </section>
                 <section
                     id="resume"
-                    className={`${currentSection === '#my_resume' ? 'block' : 'hidden'} pt-[64px]`}
+                    className={`${
+                        currentSection === '#my_resume' ? 'block' : 'hidden'
+                    } pt-[64px] pb-[115px] bg-bg-none md:bg-[url('/images/bg-shape.png')] bg-no-repeat bg-[40vw_374px]`}
                 >
-                    Resume
+                    <div className="text-center">
+                        <header className="header-has-bg mb-16">
+                            <Image
+                                src="/images/h1-bg.png"
+                                alt="h1 bg"
+                                width={166}
+                                height={75}
+                                className="header-has-bg-image"
+                            />
+                            <h1>My Resume</h1>
+                        </header>
+                        <div className="relative flex items-center justify-center z-20">
+                            <div className="flex flex-wrap w-3/4 min-w-0 h-auto">
+                                <div
+                                    id="education"
+                                    className="w-full xl:w-1/2 flex flex-col md:pr-[34px] px-3"
+                                >
+                                    <header className="header-custom mb-[42px]">
+                                        <h2 className="inline-block px-6 py-0 bg-dark_bg">
+                                            Education
+                                        </h2>
+                                    </header>
+                                    <div className="items">
+                                        <div className="flex mb-[42px] group relative md:static">
+                                            <div className="resume-item-icon">
+                                                <LiaUniversitySolid className="fill-primary" />
+                                            </div>
+                                            <div className="resume-item-box">
+                                                <span className="inline-block text-lg font-normal mb-4">
+                                                    2014 - 2018
+                                                </span>
+                                                <h2 className="text-primary mb-4">{`Engineer's degree`}</h2>
+                                                <p className="text-lg font-normal text-gray max-w-[345px]">
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="flex mb-[42px] group relative md:static">
+                                            <div className="resume-item-icon">
+                                                <LiaUniversitySolid className="las la-university" />
+                                            </div>
+                                            <div className="resume-item-box">
+                                                <span className="inline-block text-lg font-normal mb-4">
+                                                    2010 - 2014
+                                                </span>
+                                                <h2 className="mb-4">High School</h2>
+                                                <p className="text-lg font-normal text-gray max-w-[345px]">
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="flex group relative md:static">
+                                            <div className="resume-item-icon">
+                                                <LiaUniversitySolid className="las la-university" />
+                                            </div>
+                                            <div className="resume-item-box">
+                                                <span className="inline-block text-lg font-normal mb-4">
+                                                    2001 - 2010
+                                                </span>
+                                                <h2 className="mb-4">Elementary School</h2>
+                                                <p className="text-lg font-normal text-gray max-w-[345px]">
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div
+                                    id="experience"
+                                    className="w-full xl:w-1/2 flex flex-col px-3 md:pl-[34px] mt-[100px] xl:mt-0"
+                                >
+                                    <header className="header-custom mb-[42px]">
+                                        <h2 className="inline-block px-6 py-0 bg-dark_bg">
+                                            Experience
+                                        </h2>
+                                    </header>
+                                    <div className="items">
+                                        <div className="flex mb-[42px] group relative md:static">
+                                            <div className="resume-item-icon">
+                                                <BsBriefcase className="fill-primary" />
+                                            </div>
+                                            <div className="resume-item-box">
+                                                <span className="inline-block text-lg font-normal mb-4">
+                                                    2020 - Present
+                                                </span>
+                                                <h2 className="text-primary mb-4">
+                                                    Senior Web Developer
+                                                </h2>
+                                                <p className="text-lg font-normal text-gray max-w-[345px]">
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="flex mb-[42px] group relative md:static">
+                                            <div className="resume-item-icon">
+                                                <BsBriefcase className="las la-briefcase" />
+                                            </div>
+                                            <div className="resume-item-box">
+                                                <span className="inline-block text-lg font-normal mb-4">
+                                                    2019 - 2020
+                                                </span>
+                                                <h2 className="mb-4">Mid-Level Web Developer</h2>
+                                                <p className="text-lg font-normal text-gray max-w-[345px]">
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="flex group relative md:static">
+                                            <div className="resume-item-icon">
+                                                <BsBriefcase className="las la-briefcase" />
+                                            </div>
+                                            <div className="resume-item-box">
+                                                <span className="year">2017 - 2019</span>
+                                                <h2 className="mb-4">Junior Web Developer</h2>
+                                                <p className="text-lg font-normal text-gray max-w-[345px]">
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                    Bmet minim mollit deserunt llamco est sit aliqua
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
                 <section
                     id="work"
