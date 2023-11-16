904c82411782aa2b3c48cf6c7dc816acf7a76f85
ksdaylight <yuanmiwen111@163.com>
2023-11-16 06:59:20 -0500

    feat: nx config optimize

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: 'nest-api',
             script: 'node_modules/nx/bin/nx.js',
             args: 'run nest-api:serve:production',
-            instances: 2,
+            instances: 1,
             exec_mode: 'cluster',
             out_file: './logs/nest-api-out.log',
             error_file: './logs/nest-api-error.log',
